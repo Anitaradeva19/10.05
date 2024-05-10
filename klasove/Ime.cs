7f81e052d7b1b629ac3b673aa15f2df90b9b7c6b
Anitaradeva19 <anitaradeva753@gmail.com>
2024-05-10 13:26:47 +0300

    Add files via upload

---
+++ klasove/Ime.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasove
+{
+    internal class Ime
+    {
+        private string name;
+        private int age;
+
+        private string firstName;
+        private string lastName;    
+
+        public Ime(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;   
+            this.age = age; 
+        }
+        public string Name    
+        {
+            get { return name; }
+            set { name = value; }   
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
+        public void setAge(int value)
+        {
+            this.age = value;
+        }
+        public string getFirstName()
+        {
+            return this.firstName;  
+        }
+        public string setFirstName()
+        {
+            return this.firstName;
+        }
+        public string getLastName()
+        {
+            return this.lastName;   
+        }
+        public string setLastName()
+        {
+            return this.lastName;   
+        }
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} is a {this.age} years old";
+        }
+        public void Out()
+        {
+            Console.WriteLine("Zdraveite!Az sum {0} i sum  mnogo dobyr.", Name);
+        }
+    }
+}
---
+++ klasove/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines=int.Parse(Console.ReadLine());
+            var Name = new List<Ime>();
+            for(int i=0; i<lines; i++)
+            {
+                var cmArgs=Console.ReadLine();
+                var name= new Ime(cmArgs[0], cmdArds[1], int.Parse(cmdArgs[2]));
+                Name.Add(name);
+            }
+            Ime.Order();
+            
+        }
+    }
+}
